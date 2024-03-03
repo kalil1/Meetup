34b7704ad7ed4ed719bbfed6c0e4355c893d8fdb
kalil1 <wkalil416@gmail.com>
2024-03-02 23:37:43 -0500

    functional_state

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
         has_many :games
         has_one :player
         delegate :latitude, :longitude, to: :player, allow_nil: true
